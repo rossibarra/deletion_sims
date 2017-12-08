d90ebad9519ab967bbc1d8915056d6ad06b45766
Jeff Ross-Ibarra <rossibarra@gmail.com>
2017-12-08 00:53:39 -0800

    install

--- install.R
+++ install.R
@@ -1,6 +1,4 @@
 #Packages
-list.of.packages <- c("cowplot","tidyverse")
-new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
-if(length(new.packages)) install.packages(new.packages)
-lapply(list.of.packages, require, character.only = TRUE)
+install.packages("tidyverse")
+library(tidyverse)
 
