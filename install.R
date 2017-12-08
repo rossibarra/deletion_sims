c625bc05c1cc3c2ec21ba35c43d4d08acb8522e3
Jeff Ross-Ibarra <rossibarra@gmail.com>
2017-12-08 00:29:59 -0800

    commit

---
+++ install.R
@@ -0,0 +1,6 @@
+#Packages
+list.of.packages <- c("cowplot"."tidyverse")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only = TRUE)
+
